9d6ec5112e834ac94f43e1e4c761d6ab02208852
Dhruv Maheshwari <mrdm026@gmail.com>
2023-07-19 16:18:18 +0530

    Committed all the files

--- src/main/java/recipe/realm/backend/modals/Recipe.java
+++ src/main/java/recipe/realm/backend/modals/Recipe.java
@@ -2,6 +2,9 @@ package recipe.realm.backend.modals;
 
 import lombok.*;
 
+import java.util.List;
+import java.util.UUID;
+
 @Getter
 @Setter
 @ToString
@@ -9,5 +12,25 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Recipe {
+    // description
+    private UUID recipeId;
     private String recipeName;
+    private String imageURL;
+    private String summary;
+    private String sourceName;
+
+    // time
+    private RecipeTime recipeTime;
+
+    // dish type
+    private List<DishType> dishTypeList;
+
+    // instructions
+    private List<Instruction> intructionList;
+
+    // ingredients
+    private List<Ingredient> ingredientList;
+
+    // equipments
+    private List<Equipment> equipmentList;
 }
